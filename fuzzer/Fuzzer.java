0275c1f3954c548f6b902b913c529b0b83dc2f88
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2019-10-11 20:58:41 +1100

    Finish splitting

--- fuzzer/Fuzzer.java
+++ fuzzer/Fuzzer.java
@@ -61,31 +61,45 @@ public class Fuzzer {
 
 
             int numberLines = 0, tempInsSeed = 0, numberStrings = 0;
-            ArrayList <String[]>outputString = new ArrayList<String[]>();
+
+            int lessOrMore = 0;
+            ArrayList <String[]>errorProneString = new ArrayList<String[]>();
 
             //TODO:Since the invalid inputs will be generated as the last line, the randominisation will be creatd outside of the main loop and generate the inputs for the last line. 
-            while(numberLines<5){
-              tempInsSeed=generateRandomInt(0, 5);
-              String inst = INSTRUCTIONS[tempInsSeed].getOpcode();  
-              switch (tempInsSeed){
-                case PUT:
-                  numberStrings = 3; 
-                  outputString.add(genLessStrNum(PUT));
-                  break;
-                  // pw.write();
-                case GET:
-                  numberStrings = 1;
-                  break;
-                case REM:
-                  numberStrings = 1;
-                  break;
-                case LIST:
-                  numberStrings = 0;
-                  break;
-              }            
-              numberLines++;
-            }
+            tempInsSeed=generateRandomInt(0, 5);
+            // System.out.println(tempInsSeed);
+            lessOrMore = generateRandomInt(0, 1);
+            boolean isLess = true; 
+            String inst = INSTRUCTIONS[tempInsSeed].getOpcode(); 
             
+            if(lessOrMore ==0) isLess = false; 
+            tempInsSeed = 0;
+            switch (tempInsSeed){
+              case PUT:
+                System.out.println("PUT");
+                numberStrings = 3; 
+                if (isLess) errorProneString.add(genLessStrNum(PUT));
+                else System.out.println("else");
+                break;
+                // pw.write();
+              case GET:
+                System.out.println("GET");
+                numberStrings = 1;
+                errorProneString.add(genLessStrNum(GET));
+                break;
+              case REM:
+                System.out.println("REM");
+                numberStrings = 1;
+                errorProneString.add(genLessStrNum(REM));
+                break;
+              case LIST:
+                System.out.println("LIST");
+                numberStrings = 0;
+                break;
+              default:
+                break;
+            }            
+
         }catch (Exception e){
             e.printStackTrace(System.err);
             System.exit(1);
@@ -102,67 +116,117 @@ public class Fuzzer {
     // Add some randominisation. 
     
     public static int generateRandomInt(int min, int max){
+      // nextInt(least, bound): bound will be exclusive so add 1. 
       return ThreadLocalRandom.current().nextInt(min, max + 1);
     }
     public static String generateRandomStr(int seed){
       return Character.toString((char) seed);
     }
     // TODO: Refactor code, reduce copy and paste. 
-    public static String[] genLessStrNum(int inst){
-      int tempNumArgs = 0, lenOfArgs = 0, randomSeed = 0;
-      int split1=0, split2= 0;
-
-      int maxLength = 0;
-      String tempString="";
+    private static String[] genLessStrNum(int inst){
+      int tempNumArgs = 0;
 
       switch (inst){
         // Supposed to have three arguments. Generate 0 / 1 / 2 number of inputs. 
         case PUT:
           tempNumArgs = generateRandomInt(0, 2);
-          System.out.println(tempNumArgs);
-          //TODO: 这里需要-3么？？？需要减去split的空格么？？？
-          maxLength = MAX_INSTRUCTION_LENGTH-tempNumArgs - 3;
-
-          String[] returnString = new String[tempNumArgs+1];
-
-          // Generate a random string without space. 
-          while(lenOfArgs < maxLength){
-            randomSeed = generateRandomInt(33, 126);
-            // System.out.println(randomSeed);
-            tempString += generateRandomStr(randomSeed);
-            lenOfArgs ++;
-          }
-          // Split the string according to the number of args. 
-          // Return a null string.
-          if(tempNumArgs ==0){
-            tempString = " ";
-            returnString[0]= tempString;
-            return returnString;
-          }
-          // Not split.
-          else if (tempNumArgs == 1){
-            returnString[0] = tempString;
-            return returnString;
-          }
-          // Split once. 
-          else{
-            split1 = generateRandomInt(0, maxLength-1);
-            String sub = tempString.substring(0,split1);
-            String remain = tempString.substring(split1);
-            returnString[0] = sub;
-            returnString[1] = remain;
-            System.out.println(sub);
-            System.out.println(remain);
-            return returnString;
-
-          }
+          tempNumArgs = 2;
+          return splitRandomStrLess(tempNumArgs);
+        case GET:
+          tempNumArgs = 0;
+        case REM:
+          tempNumArgs = 0;
+          return splitRandomStrLess(tempNumArgs);
       }
       return null;
     }
     // Generate a random string from command. 
     // Split the string randomly and it should be invalid args. 
-    private static String splitRandomStr(int numSplit, int command){
-      return null; 
+    private static String[] splitRandomStrLess(int numArg){
+      String tempString = "";
+      String[] output = new String[numArg+1];
+      
+      int randomSeed = 0, lenOfArgs = 0, maxLength = MAX_INSTRUCTION_LENGTH-numArg;
+      int splitIndex = 0;
+      while(lenOfArgs < maxLength){
+        randomSeed = generateRandomInt(33, 127);
+            // System.out.println(randomSeed);
+        tempString += generateRandomStr(randomSeed);
+        lenOfArgs ++;
+      }
+      System.out.println(tempString.length());
+      if(numArg ==0){
+        tempString = " ";
+        output[0] = tempString;
+        return output;
+      }
+      // else if(numArg == 1){
+      //   output[0] = tempString;
+      //   return output;
+      // }
+      else{
+        splitIndex = generateRandomInt(0, maxLength-1);
+        String sub = tempString.substring(0,splitIndex);
+        String remain = tempString.substring(splitIndex);
+        output[0] = sub;
+        output[1] = remain;
+        System.out.println(tempString);
+        System.out.println(sub);
+        System.out.println(remain);
+
+        System.out.println(splitMultipleStrings(numArg, tempString));
+        System.out.println("Full String: \n"+tempString);
+        // System.out.println(splitMultipleStrings(numArg, tempString));
+
+
+        return output;
+      }
+    }
+    private static String[] splitMultipleStrings(int numArgs, String input){
+      String tempString = "", sub = ""; 
+      // TODO: 这里有时候会越界？？
+      int splitIndex = generateRandomInt(0, MAX_INSTRUCTION_LENGTH-numArgs), tempSplit=0;
+      String[] output = new String[numArgs];
+      boolean isFirstArg = true;
+      System.out.println("New Method: ");
+      System.out.println(input.length());
+      for (int i =0; i < numArgs; i++){
+        if (isFirstArg){
+          tempString = input.substring(0,splitIndex);
+          output[i] = tempString;
+          // System.out.println(output[i]);
+          isFirstArg = false;
+          System.out.println(i+" Enter first one ");
+          continue;
+        }
+        tempSplit = splitIndex;
+        // New splitindex. 
+        splitIndex = generateRandomInt(tempSplit, MAX_INSTRUCTION_LENGTH-numArgs);
+        if(i == numArgs -1){
+          tempString = input.substring(tempSplit, MAX_INSTRUCTION_LENGTH-numArgs);
+          output[i] = tempString;
+          System.out.println(i+" Enter last one ");
+          break;
+        }
+        tempString = input.substring(tempSplit,splitIndex);
+
+        output[i+1] = tempString;
+        System.out.println(i+" Enter normal one ");
+
+        // System.out.println(i+":\n"+output[i]);
+      }
+      // Traverse full split string. For testing purpose: 
+      for (int i =0; i < numArgs; i++){
+        System.out.println(i+":\n"+output[i]);
+      } 
+
+      return output;
+    }
+
+    private static String[] getMoreStrNum(int inst){
+      
+
+      return null;
     }
 }
 // TODO: Invalid inputs format: 1. 比如说input number 少了，input number多了。 2. 一行的string超过1022. 3. 总体instruction 数量超过 1024. 
