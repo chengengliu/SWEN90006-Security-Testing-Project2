4fb445e1db10acee7651561987103b2a09c1ed19
Zihan ZHANG <zhazz@student.unimelb.edu.au>
2019-10-15 01:17:23 +1100

    try to add put a b c d v4

--- fuzzer/Fuzzer.java
+++ fuzzer/Fuzzer.java
@@ -122,7 +122,7 @@ public class Fuzzer {
 				Collections.shuffle(shuffleContainer);
 				it = shuffleContainer.iterator();
 				write(it);
-				pw.println("put a b c d"); // insert an invalid instructions
+				pw.println("put a b c d"); // insert an invalid instructions to test
 				break; // invalid: insert 1025 lines of file
 			}
 
